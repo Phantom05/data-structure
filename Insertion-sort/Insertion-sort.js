23203c9d25883154afae66c94cdddc30ad3a2d7f
Phantom05 <monster2jy@gmail.com>
2019-04-12 10:50:38 +0900

    UPDATE : insertionSort update

--- Insertion-sort/Insertion-sort.js
+++ Insertion-sort/Insertion-sort.js
@@ -3,11 +3,23 @@
  */
 
 
+
+
+
+
+
+
+
+
+
+ 
 var insertionSort = function(array) {
-  var i = 1, j, temp;
-  for (i; i < array.length; i++) {
+  var temp; 
+  let j;
+  for (let i = 1 ; i < array.length; i++) {
     temp = array[i]; // 새로운 숫자를 선택함
-    for (j = i - 1; j >= 0 && temp < array[j]; j--) { // 선택한 숫자를 이미 정렬된 숫자들과 비교하며 넣을 위치를 찾는 과정, 선택한 숫자가 정렬된 숫자보다 작으면
+    for (j = i - 1; j >= 0 && temp < array[j]; j--) { 
+      // 선택한 숫자를 이미 정렬된 숫자들과 비교하며 넣을 위치를 찾는 과정, 선택한 숫자가 정렬된 숫자보다 작으면
       array[j + 1] = array[j]; // 한 칸씩 뒤로 밀어낸다
     }
     array[j + 1] = temp; // 마지막 빈 칸에 선택한 숫자를 넣어준다.
